31a78dadbcd508099b4bd97a3213f2ae6fba05ed
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-30 18:58:53 +0800

    Update restart.js

--- modules/commands/restart.js
+++ modules/commands/restart.js
@@ -1,9 +1,9 @@
 module.exports.config = {
-	name: "+",
+	name: "restart",
 	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-	description: "Restart Bot",
+	hasPermssion: 0,
+	credits: "ALVI",
+	description: "Restart the Bot",
 	commandCategory: "system",
 	usages: "",
 	cooldowns: 5
@@ -11,6 +11,5 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	const { threadID, messageID } = event;
-	return api.sendMessage(`${global.config.BOTNAME} 
-\x42\x6f\x74\x20\x61\x72\x65\x20\x6e\x6f\x77\x20\x52\x65\x73\x74\x61\x72\x74\x69\x6e\x67\x2e\x2e\x2e`, threadID, () => process.exit(1));
-  }
+	return api.sendMessage(`Wait Boss`, threadID, () => process.exit(1));
+}
