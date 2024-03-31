cba725082a0e984baafead292598cb3c6c28579b
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-31 21:52:59 +0800

    Update out.js

--- modules/commands/out.js
+++ modules/commands/out.js
@@ -1,19 +1,15 @@
 module.exports.config = {
-  name: "leave",
-  version: "1.0.0",
-  hasPermssion: 2,
-  credits: "HungCho",
-  description: "out box",
-  commandCategory: "Admin",
-  usages: "out [tid]",
-  cooldowns: 3
+    name: "out",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "Kanichi",
+    description: "Leave the group",
+    commandCategory: "Admin",
+    usages: "out [id]",
+    cooldowns: 10,
 };
 
 module.exports.run = async function({ api, event, args }) {
-    const tid = args.join(" ")
-   let namee = await api.getThreadInfo(tid)
-  if (!tid) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-
-else return api.removeUserFromGroup(api.getCurrentUserID(), tid, () => api.sendMessage("The bot has left this group", event.threadID, event.messageID));
-
+        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join("ভালোবাসার মা রে ৩৬৫ দিন ষুদি😾🖕🤖_____BOSS _ তুমি থাকো আমি চলে গেলাম🥺"));
 }
