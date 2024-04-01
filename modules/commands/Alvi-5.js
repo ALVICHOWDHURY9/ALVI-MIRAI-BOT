807a5508f6ca335bf696c8c251bcaf797ddafb8b
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-01 22:42:12 +0800

    Update and rename noprefix-mahadev.js to Alvi-5.js

---
+++ modules/commands/Alvi-5.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "🥵",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "ALVI", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "🥵",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🥵")==0 || event.body.indexOf("🥵")==0 || event.body.indexOf("🥵")==0 || event.body.indexOf("🥵")==0) {
+		var msg = {
+				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n___এমন এমন পোস্ট করবো-🤸‍♂️\n_আপনি ধরতেও পারবেন না_🥳🫡\n\n-আমি SINGLE নাকি ছ্যাকা খাইছি নাকি সন্যাসী...🙂🙂\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
+				attachment: fs.createReadStream(__dirname + `/Alvi2/alvi5.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🥵", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
