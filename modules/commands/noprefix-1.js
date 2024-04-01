f3faad9cbde54537e8f147edf50d5ad1dba7d9ea
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-01 21:20:44 +0800

    Update and rename noprefix-aaizu.js to noprefix-1.js

---
+++ modules/commands/noprefix-1.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "🙂",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "ALVI", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "🙂",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🙂")==0 || event.body.indexOf("🙂")==0 || event.body.indexOf("🙂")==0) {
+		var msg = {
+				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\nহারাম জিনিসের প্রতি এক আকাশ সমান ঘৃণা সৃষ্টি হোক__🖤\n_আমিন 🙂🤲\n\n𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
+				attachment: fs.createReadStream(__dirname + `/Alvi2/alvi.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
