4aff0d895b57f6d7418ad054c5c3b009a9445645
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-01 21:55:37 +0800

    Update and rename noprefix-ayein.js to Alvi-3.js

---
+++ modules/commands/Alvi-3.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "🤗",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "ALVI", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "🤗",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🤗")==0 || event.body.indexOf("🤗")==0 || event.body.indexOf("🤗")==0 || event.body.indexOf("🤗")==0) {
+		var msg = {
+				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n~ কেউ নিজের স্বপ্নের জন্য পরিবার ছাড়ে..!!😅\n_আবার কেউ পরিবারের জন্য নিজের স্বপ্নকে ছাড়ে_🙂\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
+				attachment: fs.createReadStream(__dirname + `/noprefix/began.mp4`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤗", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
