14e34e4f7c6be30e5a4426f0e847f2b66ba8c744
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-01 20:25:34 +0800

    Update statusvideo.js

--- modules/commands/statusvideo.js
+++ modules/commands/statusvideo.js
@@ -22,13 +22,6 @@ const fs = global.nodemodule["fs-extra"];
     var link = [
 "https://i.imgur.com/jIFYoox.mp4" , "https://i.imgur.com/w971tct.mp4" , "https://i.imgur.com/JNTP6OI.mp4" , "https://i.imgur.com/AY4eWDB.mp4" , "https://i.imgur.com/SnPt790.mp4" , "https://i.imgur.com/iQjJ5av.mp4" , "https://i.imgur.com/J6rJPXW.mp4"
      ];
-     var callback = () => api.sendMessage({body:`💝 𝗛
-
-
-
-
-
-
-𝗼𝗽𝗲 𝘆𝗼𝘂 𝗟𝗶𝗸𝗲 𝗜𝘁, \n♥️    𝗠𝗮𝗱𝗲 𝗕𝘆 𝗔𝗿𝘂𝗻🫥`,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"));
+     var callback = () => api.sendMessage({body:``,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"));
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.mp4")).on("close",() => callback());
    };
