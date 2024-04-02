22f183f8ea5f7926e4cbea23fd930e2a129daeff
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-02 18:47:49 +0800

    Update noprefix4.js

--- modules/commands/noprefix4.js
+++ modules/commands/noprefix4.js
@@ -1,24 +1,24 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "India",
+	name: "☺️",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "MrTomXxX", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
-	usages: "india",
+	usages: "☺️",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("India")==0 || event.body.indexOf("Hind")==0 || event.body.indexOf("Bharat")==0 || event.body.indexOf("desh")==0) {
+	if (event.body.indexOf("☺️")==0 || event.body.indexOf("☺️")==0 || event.body.indexOf("☺️")==0 || event.body.indexOf("☺️")==0) {
 		var msg = {
-				body: "Mera Desh Bharat🇮🇳 Jai hind 🇮🇳🇮🇳 jai bhaarat❤❤❤🇮🇳",
-				attachment: fs.createReadStream(__dirname + `/noprefix/sad/india.mp4`)
+				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n─•সম্পর্ক যতই ঘনিষ্ঠ হোক না কোনো \n-মানুষ  যে কোনো মুহূর্তে পরিবর্তন হয়ে যেতে পারে..!🌼💝\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
+				attachment: fs.createReadStream(__dirname + `/Alvi2/alvi-13.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🇮🇳", event.messageID, (err) => {}, true)
+    api.setMessageReaction("☺️", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
