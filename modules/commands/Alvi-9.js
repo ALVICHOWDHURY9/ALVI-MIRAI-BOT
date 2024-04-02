28e5abdbe49f0378fdb327b67e12e603e5eb379f
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-02 10:17:47 +0800

    Update Alvi-9.js

--- modules/commands/Alvi-9.js
+++ modules/commands/Alvi-9.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("🥲")==0 || event.body.indexOf("🥲")==0 || event.body.indexOf("🥲")==0 || event.body.indexOf("🥲")==0) {
     var msg = {
-        body: "raju",
+        body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n•--ডিপ্রেশনে থাকা মানুষগুলো জানে-🙂\n\n•প্রতিটা সেকে'ন্ড প্রতিটা রাত তাদের জন্য কি..!😭💔🥀\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
         attachment: fs.createReadStream(__dirname + `/Alvi2/alvi-9.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
