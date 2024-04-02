d187e8b56a7099d4512495d87ed8aff459df774e
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-02 09:29:35 +0800

    Update and rename noprefix-radhe.js to Alvi-6.js

---
+++ modules/commands/Alvi-6.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "🏃‍♂️",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "VanHung - Fixed By Arun", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "🏃‍♀️",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("🚶‍♀️")==0 || event.body.indexOf("🚶‍♀️")==0 || event.body.indexOf("🚶‍♂️")==0 || event.body.indexOf("radhe")==0) {
+    var msg = {
+        body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n_অনেক কিছু আশা করে_😥\n_হারিয়ে ফেলার নামই _জীবন...!🙂\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
+        attachment: fs.createReadStream(__dirname + `/Alvi2/alvi-6.mp4`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🏃‍♂️", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
