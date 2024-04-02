0da5cbb3f4cfe42fcb75be80ad4db40d76a715f6
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-02 11:18:56 +0800

    Update Alvi1.js

--- modules/commands/Alvi1.js
+++ modules/commands/Alvi1.js
@@ -13,7 +13,7 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("alvi") ||
+	if(react.includes("আলভী") ||
      react.includes("Alvi") || 
 react.includes("ALVI")) {
 		var msg = {
