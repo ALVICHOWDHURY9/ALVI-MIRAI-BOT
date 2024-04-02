0da0ff7fdec82fbc9f9c99aca59817d9c37fefe4
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-02 18:54:15 +0800

    Update Alvi-13.js

--- modules/commands/Alvi-13.js
+++ modules/commands/Alvi-13.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "💋",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "VanHung - Fixed by LTD", 
+	credits: "VanHung - Fixed by Alvi", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "💋",
