fa844c7cfe576f2ddaa87ab119b3af2cd585344f
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-02 18:41:13 +0800

    Update noprefix2.js

--- modules/commands/noprefix2.js
+++ modules/commands/noprefix2.js
@@ -1,24 +1,24 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "ishika",
+	name: "💋",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "VanHung - Fixed by LTD", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
-	usages: "Ishika",
+	usages: "💋",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Ishika")==0 || event.body.indexOf("ishika")==0 || event.body.indexOf("Didi")==0 || event.body.indexOf("Didu")==0) {
+	if (event.body.indexOf("💋")==0 || event.body.indexOf("💋")==0 || event.body.indexOf("💋")==0 || event.body.indexOf("💋")==0) {
 		var msg = {
-				body: "Wo Meri Malkin hai . Mere boss Arun ki pyari behna Ishu di😍😍",
-				attachment: fs.createReadStream(__dirname + `/noprefix/ishi.mp4`)
+				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n─•হে প্রভু...হে হরিরাম...কৃষ্ণ জগন্নাথ প্রেমানন্দী.! ইয়ে কেয়া হুয়া...!😧🤣\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
+				attachment: fs.createReadStream(__dirname + `/Alvi2/alvi-12.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("💓", event.messageID, (err) => {}, true)
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
