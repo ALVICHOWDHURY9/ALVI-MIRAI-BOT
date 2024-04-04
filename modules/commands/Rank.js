86b473d6be593041b685304fe2e64d4bdffe9533
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-04 15:29:04 +0800

    Update Rank.js

--- modules/commands/Rank.js
+++ modules/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rankcard",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "ALVI CHOWDHURY",
 	description: "",
 	commandCategory: "group",
 	cooldowns: 5,
