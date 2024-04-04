385dbf7137435b65aaa1dc684b1c97bf64beb8f3
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-04 21:30:46 +0800

    Update autoreset.js

--- modules/commands/autoreset.js
+++ modules/commands/autoreset.js
@@ -9,10 +9,10 @@ module. exports. config = {
 }
 module. exports. handleEvent = async function({ api, event, args, Users,Threads }) {
   const moment = require("moment-timezone");
-  var timeNow = moment.tz("Asia/Kolkata").format("HH:mm:ss");
+  var timeNow = moment.tz("Asia/Dhaka").format("HH:mm:ss");
   var idad = global.config.ADMINBOT;    
   console.log(timeNow)
-  var seconds = moment.tz("Asia/Kolkata").format("ss");
+  var seconds = moment.tz("Asia/Dhaka").format("ss");
   var timeRestart_1 = `07:00:${seconds}`
   var timeRestart_2 = `06:00:${seconds}`
   var timeRestart_3 = `05:00:${seconds}`
@@ -29,12 +29,12 @@ module. exports. handleEvent = async function({ api, event, args, Users,Threads 
   if ((timeNow == timeRestart_1 || timeNow == timeRestart_2 || timeNow == timeRestart_3|| timeNow == timeRestart_4|| timeNow == timeRestart_5|| timeNow == timeRestart_6 || timeNow == timeRestart_7|| timeNow == timeRestart_8|| timeNow == timeRestart_9|| timeNow== timeRestart_10|| timeNow== timeRestart_11|| timeNow == timeRestart_12) && seconds < 6 ) {
     for( let ad of idad) {
   setTimeout(() =>
-          api.sendMessage(`⚡️Abhi Time Hua hai 🗡: ${timeNow}\nBaby, Mera restart hone ka Time🙆🏻‍♀!!!`,ad, () =>process.exit(1)), 1000);
+          api.sendMessage(`🌻_বস আলভী চৌধুরী এখন টাইম হইছে_🕰️: ${timeNow}\n জানু, আমার রিস্টার্ট এর সময়_🙆‍♀️💁‍♀️`,ad, () =>process.exit(1)), 1000);
     }
     }
 }
 module. exports. run = async  ({ api, event, args }) => {
       const moment = require("moment-timezone");
-      var timeNow = moment.tz("Asia/Kolkata").format("HH:mm:ss");
+      var timeNow = moment.tz("Asia/Dhaka").format("HH:mm:ss");
         api.sendMessage(`${timeNow}`, event.threadID)
                           }
