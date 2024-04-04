9a1d632b7f20364645e0ca566bde1dd9c58d6704
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-04 09:31:24 +0800

    Update Alvi-12.js

--- modules/commands/Alvi-12.js
+++ modules/commands/Alvi-12.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "😅",
+  name: "Alvi-12",
     version: "1.0.1",
   hasPermssion: 0,
   credits: "VanHung - Fixed By Alvi", 
   description: "hihihihi",
   commandCategory: "no prefix",
-  usages: "😅",
+  usages: "alvi-12",
     cooldowns: 5, 
 };
 
