bb21ddc3658e4fed35246f52f51d4b566b59cd5d
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-04 10:47:57 +0800

    Update marriedv2.js

--- modules/commands/marriedv2.js
+++ modules/commands/marriedv2.js
@@ -69,6 +69,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n_প্রেমে পড়লে মানুষের চেহারায় উজ্জ্বলতা আসে..।🙆‍♀️\n\n-প্রে!ম একটা ফেয়ার অ্যান্ড লাভলি ক্রিম এবং স্যান্ডেলিনা সাবান...!😸💁‍♀️\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
       }
