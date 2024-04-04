0be590a2eeb72c645ae4cb515ad439c4c6d2cd50
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-04 09:36:35 +0800

    Update Alvi-12.js

--- modules/commands/Alvi-12.js
+++ modules/commands/Alvi-12.js
@@ -12,13 +12,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("😅")==0 || event.body.indexOf("😅")==0 || event.body.indexOf("😅")==0 || event.body.indexOf("😅")==0) {
+  if (event.body.indexOf("😭")==0 || event.body.indexOf("😭")==0 || event.body.indexOf("😭")==0 || event.body.indexOf("😭")==0) {
     var msg = {
         body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n\n•- জীবনে সব কিছু ছেড়ে দেওয়া যায়. !\n_কিন্তু মায়া আর ভালোবাসা.!🪶🩷\n_ছেড়ে দেওয়া যায় না!🩵🪽\n\n  𝗠𝗥. 𝗔𝗟𝗩𝗜 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬\n╰──────•◈•───────╯",
         attachment: fs.createReadStream(__dirname + `/Alvi2/alvi-11.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😅", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😭", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
