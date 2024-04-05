cfb74207df6d0b5b168cbfbf3871622ec627cc1e
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-05 15:42:33 +0800

    Update raju.js

--- modules/commands/raju.js
+++ modules/commands/raju.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "sub",
+  name: "Alvi-15",
     version: "1.0.1",
-	hasPermssion: 0,
-	credits: "VanHung - Fixed by LTD", 
-	description: "hihihihi",
-	commandCategory: "no prefix",
-	usages: "sub",
+  hasPermssion: 0,
+  credits: "VanHung - Fixed By Alvi", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "alvi-15",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("Raju")==0 || event.body.indexOf("Raju")==0 || event.body.indexOf("Raju")==0 || event.body.indexOf("@Hïgh Řăţeđ Ğabrøø")==0) {
-		var msg = {
-				body: "😍😍 Haaye i love raju , wo sirf mera hai ,wo mera Hïgh Řăţeđ Ğabrøø me uski Baby girl ..love you raju 😘😘😘r ",
-				attachment: fs.createReadStream(__dirname + `/noprefix/baby.mp4`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🤭", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("🙃")==0 || event.body.indexOf("🙃")==0 || event.body.indexOf("🙃")==0 || event.body.indexOf("🙃")==0) {
+    var msg = {
+        body: "raju",
+        attachment: fs.createReadStream(__dirname + `/Alvi2/alvi-15.mp4`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🙃", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
