9291a7902135ff46b5cc902b326f81af0e9aacbc
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-05 17:00:29 +0800

    Update trans-hindi.js

--- modules/commands/trans-hindi.js
+++ modules/commands/trans-hindi.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "hindi",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "ALVI CHOWDHURY",
 	description: "Text translation",
 	commandCategory: "media",
 	usages: "[Text]",
