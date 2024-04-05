2ead3097e3020b38da18a1cbf8e21a79a255b2cd
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-04-05 17:18:22 +0800

    Update bangladesh.js

--- modules/commands/bangladesh.js
+++ modules/commands/bangladesh.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "Bangladesh",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "VanHung - Fixed by Arun", 
+	credits: "VanHung - Fixed by Alvi", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "bangladesh ",
